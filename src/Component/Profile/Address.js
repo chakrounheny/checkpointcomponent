49a15ef65f1b3f39e75cce9cda4623ae868fa1f9
chakrounheny <chakrounpc@gmail.com>
2021-10-23 15:21:57 +0100

    hey

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const Address = () => {
+    return (
+        <div>
+            <h1>route gremda km 7 </h1>
+        </div>
+    )
+}
+
+
+export default Address
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const FullName = () => {
+    return (
+        <div>
+            <h1>henychakroun </h1>
+        </div>
+    )
+}
+
+
+export default FullName
